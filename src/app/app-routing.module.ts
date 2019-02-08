9d59aef87671c4f03402c90c83de746497425b42
oscar <obravo@inserious.com>
2019-02-08 22:32:17 +0100

    añadidas rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graph1Component } from './pages/graph1/graph1.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: LoginComponent},
+  { path: 'progress', component: ProgressComponent},
+  { path: 'graph1', component: Graph1Component},
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  { path: '**', component: NopagefoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
